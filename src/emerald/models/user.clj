e5c5d34a5d28be68774caae223a8a9b5ebd3b6d7
Alexander Juarez <couatli@gmail.com>
2015-08-05 13:49:44 -0700

    updated the oauth endpoints

--- src/emerald/models/user.clj
+++ src/emerald/models/user.clj
@@ -1,22 +1,26 @@
 (ns emerald.models.user
   (:refer-clojure :exclude [update get])
   (:use
+   [emerald.db.predicates]
    [korma.db :only (transaction)]
    [korma.core]
    [emerald.db.core])
   (:require
+   [clojure.string :as s]
    [emerald.util.password :as pwutil]))
 
-(defn get-by-email [email]
-  (-> (select users
-          (where {:email email :deleted false}))
-      first))
+(defn get-by-email-or-username [username]
+  (let [username (s/lower-case username)]
+    (-> (select users
+                (where (or {:email [ilike username]}
+                           {:name [ilike username]})))
+        first)))
 
-(defn login! [{:keys [email password] :as user}]
-  (let [{:keys [salt hashed_password] :as userstore} (get-by-email email)
+(defn login! [{:keys [username password] :as user}]
+  (let [{:keys [salt hashed_password] :as userstore} (get-by-email-or-username username)
         user (dissoc user :password)]
     (if (nil? userstore)
-      (assoc user :error "Username does not exist")
+      (assoc user :error "Username or Email does not exist")
       (if (pwutil/equals? hashed_password (str password salt))
         (dissoc userstore :hashed_password :salt)
         (assoc user :error "Password Incorrect.")))))
