91d69deec46bdc846854c3c48527cb664b6e380d
Max Winderbaum <maxwinderbaum@gmail.com>
2015-10-30 10:58:10 -0700

    finished ad tag template refactor

--- src/emerald/models/adtag.clj
+++ src/emerald/models/adtag.clj
@@ -7,5 +7,5 @@
 
 (defn all []
   (select adtags
-          (fields [:ad_tags.name :ad_tag_templates.template])
+          (fields :ad_tags.id :ad_tags.name :ad_tags.player_code :ad_tags.version_suffix :ad_tags.is_in_stream :ad_tags.attributes :ad_tag_templates.template)
           (join adtag-templates)))
